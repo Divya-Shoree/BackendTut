a86ca9619cbe3aac186f6ace399410ab6d05ae49
ShoreeDivya <divyashoree@gmail.com>
2023-12-28 20:29:36 -0500

    register controller tested and bugs fixed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
+      lowecase: true,
       trim: true,
     },
     fullname: {
@@ -26,11 +26,11 @@ const userSchema = new Schema(
       index: true,
     },
     avatar: {
-      type: String, //cloudinary url
+      type: String, // cloudinary url
       required: true,
     },
     coverImage: {
-      type: String, //cloudinary url
+      type: String, // cloudinary url
     },
     watchHistory: [
       {
@@ -46,11 +46,14 @@ const userSchema = new Schema(
       type: String,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) return next();
+  if (!this.isModified("password")) return next();
+
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
@@ -60,26 +63,29 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 };
 
 userSchema.methods.generateAccessToken = function () {
-  return jwt.sign({
-    _id: this._id,
-    email: this.email,
-    username: this.username,
-    fullname: this.fullname,
-  });
-  process.env.ACCESS_TOKEN_SECRET,
+  return jwt.sign(
     {
-      expiresIn: ACCESS_TOKEN_EXPIRY,
-    };
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
 };
-
 userSchema.methods.generateRefreshToken = function () {
-  return jwt.sign({
-    _id: this._id,
-  });
-  process.env.REFRESH_TOKEN_SECRET,
+  return jwt.sign(
     {
-      expiresIn: REFRESH_TOKEN_EXPIRY,
-    };
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 
 export const User = mongoose.model("User", userSchema);
